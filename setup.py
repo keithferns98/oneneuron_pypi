48fbcd82bd872501eb8e519c8b6fbf3a45cd2e2b
Keith <keithfernandes311#gmail.com>
2021-09-20 18:31:11 +0530

    some changes

--- setup.py
+++ setup.py
@@ -3,17 +3,19 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+Project_Name="oneneuron_pypi"
+User_Name="keithferns98"
 setuptools.setup(
-    name="keithferns98-OneNeuron",
+    name=f"{Project_Name}-{User_Name}",
     version="0.0.1",
-    author="Keith",
+    author="User_Name",
     author_email="keithfernandes311@gmail.com",
     description="Perceptron package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/keithferns98/OneNeuron_pypi",
+    url="https://github.com/keithferns98/oneneuron_pypi",
     project_urls={
-        "Bug Tracker": "https://github.com/keithferns98/OneNeuron_pypi/issues",
+        "Bug Tracker": "https://github.com/keithferns98/oneneuron_pypi/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
